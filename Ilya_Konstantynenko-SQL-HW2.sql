26c9fd1de5842531cdc3d455dae9b74a3c490ad6
ikonsty <i.konsty@gmail.com>
2021-03-22 13:32:06 +0200

    first commit

--- Ilya_Konstantynenko-SQL-HW2.sql
+++ Ilya_Konstantynenko-SQL-HW2.sql
@@ -2,7 +2,8 @@
 -- createdb -U postgres -w encyclopedia
 -- psql -U postgres -w -d encyclopedia
 
---               *** YOUR CODE HERE ***
+	$ createdb encyclopedia
+	$ psql -d encyclopedia
 
 -- 2. Create a table called countries. It should have the following columns:
 --     An id column of type serial
@@ -13,7 +14,12 @@
 -- The name column should have a UNIQUE constraint.
 -- The name and capital columns should both have NOT NULL constraints.
 
---               *** YOUR CODE HERE ***
+	CREATE TABLE countries (
+	    id SERIAL,
+	    name VARCHAR(50) UNIQUE NOT NULL,
+	    capital VARCHAR(50) NOT NULL,
+	    population INT
+	);
 
 -- 3.  Create a table called famous_people. It should have the following columns:
 --     An id column that contains auto-incrementing values
@@ -25,8 +31,13 @@
 -- The table should prevent NULL values being added to the name column.
 -- If the value of the deceased column is unknown then false should be used.
 
---               *** YOUR CODE HERE ***
-
+	CREATE TABLE famous_people (
+		id SERIAL UNIQUE
+		name VARCHAR(100) NOT NULL,
+		occupation VARCHAR(150),
+		date_of_birth VARCHAR(50),
+		deceased BOOLEAN DEFAULT false
+	);
 -- 4. Create a table called animals that could contain the sample data below:
 -- |name 	        |binomial_name 	    |max_weight_kg 	    |max_age_years 	    |conservation_status
 -- ------------------------------------------------------------------------------------------------
@@ -53,30 +64,42 @@
 --     the 'precision', which is the total number of digits on both sides of the decimal point,
 --     and the 'scale', which is the number of digits to the right of the decimal point.
 
---               *** YOUR CODE HERE ***
-
+	CREATE TABLE animals (
+		id SERIAL UNIQUE,
+		name VARCHAR(100) NOT NULL,
+		binomial_name VARCHAR(100) NOT NULL,
+		max_weight_kg DECIMAL(8,3),
+		max_age_years INT(),
+		conservation_status CHAR(2)
+	);
 -- 5. List all of the tables in the encyclopedia database.
 
---               *** YOUR CODE HERE ***
+	$ encyclopedia=# \dt
 
 -- 6. Display the schema for the animals table.
 
---               *** YOUR CODE HERE ***
+	$ encyclopedia=# \d animals
 
 -- 7. Rename the famous_people table to celebrities.
 
---               *** YOUR CODE HERE ***
+	ALTER TABLE famous_people
+	RENAME TO celebrities;
 
 -- 8. Change the name of the name column in the celebrities table to first_name, and change its data type to varchar(80).
 -- The order in which you execute the statements isn't important.
-
---               *** YOUR CODE HERE ***
+	
+	ALTER TABLE celebreties
+	RENAME COLUMN name TO first_name;
+	
+	ALTER TABLE celebreties
+	MODIFY COLUMN first_name VARCHAR(80);
 
 -- 9. Create a new column in the celebrities table called last_name.
 -- It should be able to hold strings of lengths up to 100 characters.
 -- This column should always hold a value.
-
---               *** YOUR CODE HERE ***
+	
+	ALTER TABLE celebreties
+	ADD last_name VARCHAR(100) NOT NULL;
 
 -- 10. Change the celebrities table so that the date_of_birth column uses a data type that holds
 -- an actual date value rather than a string. Also ensure that this column must hold a value.
@@ -90,8 +113,11 @@
 --      ALTER COLUMN column_name
 --          TYPE new_data_type
 --          USING column_name::new_data_type
-
---               *** YOUR CODE HERE ***
+	
+	ALTER TABLE celebreties
+	ALTER COLUMN date_of_birth
+		TYPE DATE
+		USING date_of_birth::DATE;
 
 -- 11. Change the animals table so that the binomial_name column cannot contain duplicate values.
 -- You will need to use the table constraint form and specify a particular column as part of the constraint clause:
@@ -99,14 +125,16 @@
 --      ALTER TABLE table_name
 --      ADD CONSTRAINT constraint_name CONSTRAINT_TYPE (column_name);
 
---               *** YOUR CODE HERE ***
+	ALTER TABLE animals
+	ADD CONSTRAINT UC_name UNIQUE (binomial_name);
 
 -- 12. Add the following data to the countries table:
 --      |name 	    |capital 	|population
 -------------------------------------------
 --      |France 	|Paris 	    |67,158,000
 
---               *** YOUR CODE HERE ***
+	INSERT INTO countries (name, capital, population)
+	VALUES ('France', 'Paris', 67158000);
 
 -- 13. Now add the following additional data to the countries table:
 --      |name 	    |capital 	        |population
@@ -115,18 +143,27 @@
 --      |Germany    |Berlin 	        |82,349,400
 --      |Japan 	    |Tokyo 	        |126,672,000
 
---               *** YOUR CODE HERE ***
+	INSERT INTO countries (name, capital, population)
+	VALUES ('USA', 'Washington D.C.', 325365189);
+	
+	INSERT INTO countries (name, capital, population)
+	VALUES ('Germany', 'Berlin', 382349400);
+	
+	INSERT INTO countries (name, capital, population)
+	VALUES ('Japan', 'Tokyo', 126672000);
 
 -- 14. Add an entry to the celebrities table for the singer and songwriter
 -- Bruce Springsteen, who was born on September 23rd 1949 and is still alive.
 
---               *** YOUR CODE HERE ***
+	INSERT INTO celebreties (first_name, last_name, occupation, date_of_birth)
+	VALUES ('Bruce', 'Springsteen', 'singer and songwriter', '1949-09-23');
 
 -- 15.  Add an entry for the actress
 -- Scarlett Johansson, who was born on November 22nd 1984.
 -- Use the default value for the deceased column.
 
---               *** YOUR CODE HERE ***
+	INSERT INTO celebreties (first_name, last_name, occupation, date_of_birth)
+	VALUES ('Scarlett', 'Johansson', 'actress', '1984-11-22');
 
 -- 16. Add the following two entries to the celebrities table with a single INSERT statement.
 --      For Frank Sinatra set true as the value for the deceased column.
@@ -137,7 +174,8 @@
 -- |Frank 	    |Sinatra 	|Singer, Actor 	|December 12, 1915
 -- |Tom 	    |Cruise 	|Actor 	        |July 03, 1962
 
---               *** YOUR CODE HERE ***
+	INSERT INTO celebreties (first_name, last_name, occupation, date_of_birth, deceased)
+	VALUES ('Frank', 'Sinatra', 'Singer, Actor', '1915-12-12', true), ('Tom', 'Cruise', 'Actor', '1962-07-03');	
 
 -- 17.  Look at the schema of the celebrities table.
 --      What do you think will happen if we try to insert the following data?
@@ -146,14 +184,18 @@
 --      |Madonna 		|           |Singer, Actress 	                 |'08/16/1958' 	|false
 --      |Prince 		|           |Singer, Songwriter, Musician, Actor |'06/07/1958' 	|true
 
---               *** YOUR CODE HERE ***
+--		Something like: NOT NULL constraint failed: celebreties.last_name
 
 -- 18. Update the last_name column of the celebrities table so that the data in the previous question can be entered, and then add the data to the table.
 -- Hint:
 --      First we need to alter the table column to remove the NOT NULL constraint:
 --      Then we can insert the data.
 
---               *** YOUR CODE HERE ***
+	ALTER TABLE celebreties
+	DROP CONSTRAINT UC_last_name;
+	
+	INSERT INTO celebreties (first_name, occupation, date_of_birth, deceased)
+	VALUES ('Madonna', 'Singer, Actress', '1958-08-16', false), ('Prince', 'Singer, Songwriter, Musician, Actor', '1958-06-07', true);	
 
 -- 19. Check the schema of the celebrities table.
 -- What would happen if we specify a NULL value for deceased column, such as with the data below?
@@ -162,7 +204,9 @@
 -----------------------------------------------------------------------------------------
 -- |Elvis 	    |Presley 	|Singer, Musician, Actor 	|'01/08/1935' 	|NULL
 
---               *** YOUR CODE HERE ***
+	$ encyclopedia=# \d celebreties
+	
+--  error would occured because bool can not contain NULL
 
 -- 20. Check the schema of the animals table.
 -- What would happen if we tried to insert the following data to the table?
@@ -177,15 +221,25 @@
 -- |Pigeon 	         |Columbidae Columbiformes 	|2 	            	|15 	            |LC
 -- |Kakapo 	         |Strigops habroptila 	    |4 	            	|60 	            |CR
 
---               *** YOUR CODE HERE ***
+	$ encyclopedia=# \d animals
+--	there is two same binomial names so we need to remove UNIQUE for this column
+	ALTER TABLE animals
+	DROP CONSTRAINT UC_binomial_name;
+	
+	INSERT INTO animals (name, binomial_name, max_weight_kg, max_age_years, conservation_status)
+	VALUES ('Dove', 'Columbidae Columbiformes', 2, 15, 'LC'), ('Golden Eagle', 'Aquila Chrysaetos', 6.35, 24, 'LC'),
+	('Peregrine Falcon', 'Falco Peregrinus', 1.5, 15, 'LC'), ('Pigeon', 'Columbidae Columbiformes', 2, 15, 'LC'), 
+	('Kakapo', 'Strigops habroptila', 4, 60, 'CR')
+	
 
 -- 21.Create database ls_burger.
 
---               *** YOUR CODE HERE ***
+	$ encyclopedia=# CREATE DATABASE ls_burger
 
 -- 22. Connect to ls_burger.
 
---               *** YOUR CODE HERE ***
+	$ encyclopedia=# \c ls_burger
+	$ ls_burger=#
 
 -- 23. Create a table in the ls_burger database called orders. The table should have the following columns:
 --
@@ -200,22 +254,42 @@
 -- For the order_total column, our decimal needs a precision of 4, since the maximum value of an order is theoretically $99.99.
 -- The scale is 2, since the two digits to the right of the decimal represent the number of cents.
 
---               *** YOUR CODE HERE ***
+	CREATE TABLE orders (
+	  id SERIAL UNIQUE,
+	  customer_name VARCHAR(100) NOT NULL,
+	  burger VARCHAR(50),
+	  side VARCHAR(50),
+	  drink VARCHAR(50),
+	  order_total DECIMAL(4,2) NOT NULL
+	);
 
 -- 24. Add the following columns to the orders table:
 --     A column called customer_email; it should hold strings of up to 50 characters.
 --     A column called customer_loyalty_points that should hold integer values. If no value is specified for this column, then a value of 0 should be applied.
 
---               *** YOUR CODE HERE ***
+	ALTER TABLE orders
+	ADD customer_email VARCHAR(50);
+	
+	ALTER TABLE orders
+	ADD customer_loyalty_points INT() DEFAULT 0;
 
 -- 25. Add three columns to the orders table called burger_cost, side_cost, and drink_cost to hold monetary values in dollars and cents (assume that all values will be less than $100).
 -- If no value is entered for these columns, a value of 0 dollars should be used.
 
---               *** YOUR CODE HERE ***
+	ALTER TABLE orders
+	ADD burger_cost DECIMAL(4,2) DEFAULT 0;
+	
+	ALTER TABLE orders
+	side_cost DECIMAL(4,2) DEFAULT 0;
+	
+	
+	ALTER TABLE orders
+	drink_cost DECIMAL(4,2) DEFAULT 0;
 
 -- 26. Remove the order_total column from the orders table.
 
---               *** YOUR CODE HERE ***
+	ALTER TABLE orders
+	DROP order_total;
 
 -- 27. Based on the table schema and following information, write and execute an INSERT statement to add the appropriate data to the orders table.
 --  There are three customers - James Bergman, Natasha O'Shea, Aaron Muller.
@@ -245,4 +319,9 @@
 --      Where an order doesn't include a particular item (burger, side, or drink) we have to specify a NULL value for the appropriate column.
 --      For the equivalent cost column, we could either explicitly use the DEFAULT or specify a value of 0.00 or 0.
 
---               *** YOUR CODE HERE ***
+	INSERT INTO orders (customer_name, customer_email, burger, side, drink, customer_loyalty_points, burger_cost, side_cost, drink_cost)
+	VALUES ('James Bergman', 'james1998@email.com', 'LS Chicken Burger', 'Fries', 'Cola', 28, 4.50, 0.99, 1.50), 
+	('Natasha O`Shea', 'natasha@osheafamily.com', 'LS Cheeseburger', 'Fries', null, 18, 3.50, 0.99), 
+	('Natasha O`Shea', 'natasha@osheafamily.com', 'LS Double Deluxe Burger', 'Onion Rings', 'Chocolate Shake', 42, 6.00, 1.50, 2.00),
+	('Aaron Muller', null, 'LS Burger', null, null, 10, 3.00)
+	
